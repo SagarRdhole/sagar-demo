4b86a51abd3190859602dbfb57fa1d25b4211c91
sagar@012345 <dholesagar84@gmail.com>
2024-02-04 23:43:51 +0530

    new changes for deployment.yml

--- raj.py
+++ raj.py
@@ -1,3 +1,17 @@
-nksdcjjdlnldsnodsh,msd kjs
-dnskjnsnu
- kdsjldnm skn
+apiVersion: apps/v1
+kind: Deployment
+metadata: 
+  name: fusion-labs-frontend
+spec: 
+  replicas: 5
+  selector: 
+    matchLabels: 
+      tier: fusion-labs-frontend
+  template:
+    metadata:  
+      labels: 
+        tier: fusion-labs-frontend
+    spec: 
+      containers: 
+       - name: nginx-service
+         image: nginx
